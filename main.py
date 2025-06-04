e03663c65f50d65b42b34e4e4da299032f5f4a06
Kyrylo Stadniuk <krllstdn@gmail.com>
2025-06-04 18:58:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+def align(seq1, seq2, match=1, mismatch=-1, gap=-2):
+    """
+    Needleman-Wunsch DP algo
+    """
+    m, n = len(seq1), len(seq2)
+    
+    # init score matrix F and traceback matrix T
+    F = [[0] * (n + 1) for _ in range(m + 1)]
+    T = [[None] * (n + 1) for _ in range(m + 1)]  # 'diag', 'up', 'left'
+
+    # init first row and column
+    for i in range(1, m + 1):
+        F[i][0] = i * gap
+        T[i][0] = 'up'
+    for j in range(1, n + 1):
+        F[0][j] = j * gap
+        T[0][j] = 'left'
+
+    # fill in the matrix
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            s = match if seq1[i - 1] == seq2[j - 1] else mismatch
+            diag = F[i - 1][j - 1] + s
+            up = F[i - 1][j] + gap
+            left = F[i][j - 1] + gap
+            F[i][j] = max(diag, up, left)
+
+            # traceback
+            if F[i][j] == diag:
+                T[i][j] = 'diag'
+            elif F[i][j] == up:
+                T[i][j] = 'up'
+            else:
+                T[i][j] = 'left'
+
+    # get the alignment
+    aligned1, aligned2 = [], []
+    i, j = m, n
+    while i > 0 or j > 0:
+        if T[i][j] == 'diag':
+            aligned1.append(seq1[i - 1])
+            aligned2.append(seq2[j - 1])
+            i -= 1
+            j -= 1
+        elif T[i][j] == 'up':
+            aligned1.append(seq1[i - 1])
+            aligned2.append('-')
+            i -= 1
+        else:  # 'left'
+            aligned1.append('-')
+            aligned2.append(seq2[j - 1])
+            j -= 1
+    return F[m][n], ''.join(reversed(aligned1)), ''.join(reversed(aligned2))
+
+seq1 = "ACGTGCTAGCTAGCTAGGCTAGCTACGTAGCTAGCTAGCA"
+seq2 = "ACGTCGATGCTAGATAGGATAGCTCGGCTAAGGTAGCTAGTTAGCA"
+
+score, a1, a2 = align(seq1, seq2)
+print("Score:", score)
+print(a1)
+print(a2)
+
+
